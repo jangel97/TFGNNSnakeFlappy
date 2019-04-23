95e7c35de19880a953203759cc7e51c085899774
joseangel.morena <joseangel.morena@estudiants.urv.cat>
2019-04-23 23:52:38 +0200

    Arreglos flappy.py

--- games/flappy.py
+++ games/flappy.py
@@ -21,30 +21,25 @@ vectors_and_keys = [
                 [[0, 1], 1]
                 ]  
 
+
 def generate_action(request):
 	global movimientos
 	print "MOVIMIENTOS: " + str(movimientos)
-	# En el entrenamiento hay 2 tipos de entrenamientos
-	# Si los movimientos<700 es un entrenamiento algoritmico intenta no fallar 
-	# si >700 i <800 siempre va hacia arriba ya que en el entrenamiento algoritmico no siempre hay ocasiones por arriba del agujero.
 	if movimientos<700:
 		global indice
 		movimientos=movimientos+1
 		flappy=request["bird"]
 		pipes=request["pipes"]
-		a=randint(0,2)
-		# Jugamos con la aleatoriedad para hacer que el pajaro falle sino el entrenamiento sería infinito
+		a=randint(0,2)	#JUGAR CON ALEATORIEDAD PODRIA SER INTERESANTE PARA EL TEMA DEL ENTRENO
 		if a==1 :	
 			try:
-				# miramos si el flappy esta pord debajo del punto medio del agujero
+				#print flappy["y"], pipes[1]["y"]-60
 				if flappy["y"]>=pipes[indice]["y"]-60:
-					return 1,vectors_and_keys[1][0] # devolvemos 1 para saltar
+					return 1,vectors_and_keys[1][0]
 				else:
-					return 0,vectors_and_keys[0][0] # devolvemos 0 para dejar caer
+					return 0,vectors_and_keys[0][0]
 	
 			except:
-				# esto es para el inicio del flappy ya que no hay pipies hacemos una probabilidad 
-				# para que el pajaro se mantenga a flote hasta que hay un pipie y reacciona
 				global inicio 
 				inicio=(inicio+1)%17
 
@@ -54,13 +49,15 @@ def generate_action(request):
 					return 1,1
 		else:
 			return 0,0
+			
+		
 	else:
-		# si >800 reseteamos el entreno
 		if movimientos>800:
 			movimientos=0
 		else:
 			movimientos=movimientos+1
-		# devolvemos siempre 1 para ir hacia arriba
+		print "EMPIEZO ENTRENO ARRIBA"
+
 		return 1,vectors_and_keys[1][0]
 		
 
@@ -68,11 +65,11 @@ def generate_observation(request):
 	flappy=request["bird"]
 	pipes=request["pipes"]
 	score=request["score"]
-	# llamamos la funcion que nos devolvera todos los obs de la situacion
+	
 	verticalARRIBA, verticalABAJO,horizontalPRIMERA,horizontalSEGUNDA=get_positions(flappy,pipes,score)
 	#print pipes
 	#print pipes[0]["y"], pipes[1]["y"], flappy["y"], vertical
-	return [verticalARRIBA, verticalABAJO,horizontalPRIMERA,horizontalSEGUNDA] #devolvemos los obs
+	return [verticalARRIBA, verticalABAJO,horizontalPRIMERA,horizontalSEGUNDA]
 
 def get_positions(flappy,pipes,score):
 	global indice
@@ -84,7 +81,6 @@ def get_positions(flappy,pipes,score):
 	global h
 
 	#print indice, prev_score, auxiliar
-	#Miramos si el score anterior es menor para poder actualizarlo
 	if prev_score < score:
 		if auxiliar>40:
 			prev_score=score
@@ -96,28 +92,47 @@ def get_positions(flappy,pipes,score):
 		prev_v=v
 		prev_h=h
 		print "PIPEEEEEEEEEE:" +str(pipes)
-		#cojemos los vectores a todos los vertices del agujero por donde tiene que pasar el pajaro respecto el pajaro
 		hcerca=pipes[indice]["x"]-flappy["x"]
-		v=pipes[indice]["y"]-120-flappy["y"] 
+		v=pipes[indice]["y"]-120-flappy["y"] #pipes[0]=top pipes[1]=bot (considerar el punto medio ancho del hole=120 sumar 60 para punto medio)
 		vcerca=pipes[indice]["y"]-flappy["y"]
-		h=pipes[indice]["x"]+60-flappy["x"]
+		h=pipes[indice]["x"]+60-flappy["x"] #50 antes
 
 		return v,vcerca,h,hcerca
 
-	except (Exception) as error:
+	except (Exception) as error:	#PETA JUSTO AL PASAR POR UN PIPE
 		print (error)
 		return 0,0,0,0
 
-def reset():
+def resetflappy():
 	global indice
 	global prev_score
 	global auxiliar
-	# reseteamos los valores para poder volver a jugar
+
 	indice=1
 	prev_score=0
 	auxiliar=0
 
-def wasGoodAction(request):
+def get_flappy_direction_vector(flappy):
+	global anterior
+	if anterior is []:
+		anterior=flappy
+                #print "ANTERIOOOOOR: " + anterior
+		return [0,0]	#!!!!
+	else:
+		direction = np.array(flappy) - np.array(anterior)	#vector direccion del flappy
+		anterior=flappy
+		return direction
+
+
+def get_game_action_predict(request, action):
+	#print 'REQUEST: ' + str(request) + '\n'
+	#print 'ACTION Q VIENE: ' + str(action)
+	if (action==1):
+		return [0,1]
+	else:
+		return [0,-1]
+
+def wasGoodActionFlappy(request):
 	global prev_score
 	global prev_v
 	global v
@@ -129,16 +144,26 @@ def wasGoodAction(request):
 	flappy=request["bird"]
 	score=request["score"]
 	pipes=request["pipes"]
-	
-	#si el score ha mejorado o el pajaro esta dentro el rango del agujero por donde tiene que pasar devolvera true
-	if (prev_score<score or (pipes[indice]["y"]>flappy["y"]  and pipes[indice]["y"]-120<flappy["y"])):
+	'''
+	if (pipes[indice]["y"]<flappy["y"]):	#si el pajaro esta por debajo del agujero
+			#return (flappy["y"]<prev_v or prev_score<score )
+			return (flappy["y"]<prev_v)
+		elif (pipes[indice]["y"]-120>flappy["y"]):
+			#return (flappy["y"]>prev_v or prev_score<score)
+			return (flappy["y"]>prev_v)
+	'''
+	#PROPUESTA JOSE	
+		
+	if (prev_score<score or (pipes[indice]["y"]>flappy["y"]  and pipes[indice]["y"]-120<flappy["y"])):	#TENER EN CUENTA HORIZONTAL
+		#return (not(request["done"]) or prev_score<score) 	 #NO ALEJARSE DEL PUNTO MEDIO
 		return True
 	else:
-		if (pipes[indice]["y"]-20<flappy["y"]):
-			#si el pajaro esta por debajo del agujero
+		if (pipes[indice]["y"]-20<flappy["y"]):	#si el pajaro esta por debajo del agujero
+			#return (flappy["y"]<prev_v or prev_score<score )
 			return (flappy["y"]<prev_v)
 		elif (pipes[indice]["y"]-110>flappy["y"]):
-			#si el pajaro esta por arriba del agujero
+			#return (flappy["y"]>prev_v or prev_score<score)
 			return (flappy["y"]>prev_v)
 	
+
 
